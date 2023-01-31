944ab4ebf238eab950a0c1bcc071cb7fa97bd67e
yashhhere <mishra.y19@gmail.com>
2023-01-31 16:54:13 +0530

    Initial commit

---
+++ src/main/java/com/yashscode/crick_game/CrickGameApplication.java
@@ -0,0 +1,13 @@
+package com.yashscode.crick_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrickGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrickGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yashscode/crick_game/CrickGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashscode.crick_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrickGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
